e1be3aedf395acc43a65fabc1894734aedf65f56
Itheans <eark2207@hotmail.com>
2024-11-12 00:06:08 +0700

    gg

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:myproject/pages.dart/buttomnav.dart';
+import 'package:myproject/pages.dart/chat.dart';
+import 'package:myproject/pages.dart/chatpage.dart';
+import 'package:myproject/pages.dart/home.dart';
+import 'package:myproject/pages.dart/login.dart';
+import 'package:myproject/pages.dart/onboard.dart';
+import 'package:myproject/pages.dart/sigup.dart';
+import 'package:myproject/services/auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: FutureBuilder(
+            future: AuthMethods().getCurrentUser(),
+            builder: (context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.hasData) {
+                return Sigup();
+              } else {
+                return Sigup();
+              }
+            }));
+  }
+}
